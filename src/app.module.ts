1783d6a1ab46c9e2927baea38a2f12efbc58d46c
Arjunkumar1877 <arjun.tech177@gmail.com>
2024-12-08 17:50:52 +0530

    Added task module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TaskService } from './task/task.service';
+import { TaskModule } from './task/task.module';
 
 @Module({
-  imports: [],
+  imports: [
+   TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: '7025',
+    database: 'db_test',
+    autoLoadEntities: true,
+    synchronize: true   
+   }),
+   TaskModule
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, TaskService],
 })
 export class AppModule {}
