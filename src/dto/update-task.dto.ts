3d28e6dbc4e42603a0d20fe3d76528875f25d1fb
Arjunkumar1877 <arjun.tech177@gmail.com>
2024-12-08 18:00:15 +0530

    Added dto and task service

---
+++ src/dto/update-task.dto.ts
@@ -0,0 +1,17 @@
+import { IsBoolean, IsOptional, IsString } from "class-validator";
+
+
+export class UpdateTaskDto{
+    @IsOptional()
+    @IsString()
+    title?: string;
+
+
+    @IsOptional()
+    @IsString()
+    description?: string;
+
+    @IsOptional()
+    @IsBoolean()
+    completed?: boolean;
+}
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,4 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Task } from './task.entity';
+import { CreateTaskDto } from 'src/dto/create-task.dto';
+import { UpdateTaskDto } from 'src/dto/update-task.dto';
 
 @Injectable()
-export class TaskService {}
+export class TaskService {
+    constructor(
+        @InjectRepository(Task)
+        private taskRepository: Repository<Task>,
+    ){}
+
+    async findOne(id: number): Promise<Task>{
+        const task = await this.taskRepository.findOne({where: {id}});
+
+        if(!task){
+            throw new NotFoundException(`Task with ID ${id} not found`);
+        }
+     return task;
+    }
+  
+  async create(createTaskDto: CreateTaskDto): Promise<Task>{
+    const task = this.taskRepository.create(createTaskDto);
+    return this.taskRepository.save(task);
+  }
+
+  async update(id: number, updateTaskDto: UpdateTaskDto): Promise<Task>{
+    const task = await this.findOne(id);
+    Object.assign(task,  updateTaskDto);
+    return this.taskRepository.save(task);
+  }
+
+  async delete(id: number): Promise<void>{
+    const result = await  this.taskRepository.delete(id);
+    if(result.affected  === 0){
+        throw new NotFoundException(`Task with ID ${id} not fount`)
+    }
+  }
+
+}
